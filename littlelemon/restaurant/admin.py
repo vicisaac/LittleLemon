152a86c35f21c89ef3a017873c4f0c381bb835bd
vicisaac <61437599+vicisaac@users.noreply.github.com>
2026-01-01 23:56:11 +0100

    added Models for Booking and menu

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Booking, Menu
 
-# Register your models here.
+# Register models with the admin interface
+admin.site.register(Booking)
+admin.site.register(Menu)
--- littlelemon/restaurant/models copy.py
+++ littlelemon/restaurant/models copy.py
@@ -8,6 +8,10 @@ class Booking(models.Model):
     no_of_guests = models.IntegerField()     # Number of guests for the booking
     booking_date = models.DateTimeField()    # Date and time of the booking
 
+    # Meta information (optional, can help in defining DB constraints)
+    class Meta:
+        db_table = 'booking'  # The table name in the database (Django defaults to using the model's name)
+
     # String representation of the object
     def __str__(self):
         return f"Booking for {self.name} on {self.booking_date}"
@@ -15,9 +19,13 @@ class Booking(models.Model):
 # Menu model
 class Menu(models.Model):
     # Fields
-    title = models.CharField(max_length=355)                         # Name or title of the menu item
-    price = models.DecimalField(max_digits=10, decimal_places=2)     # Price of the menu item
-    inventory = models.IntegerField()                                # Quantity in stock of the menu item
+    title = models.CharField(max_length=355)  # Name or title of the menu item
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Price of the menu item
+    inventory = models.IntegerField()  # Quantity in stock of the menu item
+
+    # Meta information (optional, can help in defining DB constraints)
+    class Meta:
+        db_table = 'menu'  # The table name in the database (Django defaults to using the model's name)
 
     # String representation of the object
     def __str__(self):
